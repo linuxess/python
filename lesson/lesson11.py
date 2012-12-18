53fb02823c893e637f59ad8ef636ac5789f8ac7d
linuxess <linuxess@163.com>
2012-12-18 11:07:12 +0800

    change lesson11

--- lesson/lesson11.py
+++ lesson/lesson11.py
@@ -19,4 +19,12 @@ so, you'er 23 old, 158 tall.
 print 后面加了个逗号(comma) print 就不会输出新行符而结束这一行跑到下一行去了。
 """
 
+"""
+改进版：
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+
+print "so, you'er %s old, %s tall." % (age,height)
+"""
+
 
