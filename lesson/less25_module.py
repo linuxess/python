64393158e452a84747a0c3db18c6da867759fc47
linuxess <linuxess@163.com>
2012-12-20 11:50:12 +0800

    write and import moudle

---
+++ lesson/less25_module.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+# ScriptName: less25_module.py
+
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split()
+    return words
+
+def sort_words(words):
+    """sorts the words"""
+    return sorted(words)
+
+def print_first_word(words):
+    """print the first word after popping it off"""
+    word = words.pop(0)
+    print word
+
+def print_last_word(words):
+    """print the last word after poping if off"""
+    word = words.pop(-1)
+    print word
+
+def sort_sentence(sentence):
+    """takes in a full sentence and returns the sorted words."""
+    words = break_word(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+
+"""
+>>> sentence = "All good things come to those who wait."
+>>> import less25_module
+>>> sentence = "All good things come to those who wait."
+>>> words = less25_module.break_words(sentence)
+>>> words
+['All', 'good', 'things', 'come', 'to', 'those', 'who', 'wait.']
+>>> sorted_words = less25_module.sort_words(words)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> less25_module.print_first_word(sorted_words)
+All
+>>> less25_module.print_last_word(sorted_words)
+who
+>>> less25_module.print_first_and_last(sentence)
+All
+wait.
+"""
