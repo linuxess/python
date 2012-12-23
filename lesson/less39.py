49d53013e201169461a79919385953021d58078c
linuxess <linuxess@163.com>
2012-12-23 15:29:36 +0800

    dict &&  class

--- lesson/less39.py
+++ lesson/less39.py
@@ -3,6 +3,8 @@
 # Function:Dictionaries, Oh Lovely Dictionaries 
 # ScriptName: less39.py
 #***************************************************************#
+#如果说类和迷你模块差不多，那么对于类来说，也必然有一个类似 import 的概念。这个概念名称就是“实例(instance)”。
+#这只是一种故作高深的叫法而已，它的意思其实是“创建”。当你将一个类“实例化”以后，你就得到了一个 对象(object) 。
 
 class song(object):
     
@@ -34,3 +36,9 @@ They rally around the family
 With pockets full of shells
 """
 
+"""
+为什么创建 __init__ 或者别的类函数时需要多加一个 self 变量？
+
+如果你不加 self ， cheese = 'Frank' 这样的代码意义就不明确了，它指的既可能是实例的 cheese 属性，或者一个叫做 cheese 的局部变量。
+有了 self.cheese = 'Frank' 你就清楚地知道了这指的是实例的属性 self.cheese 。
+"""
